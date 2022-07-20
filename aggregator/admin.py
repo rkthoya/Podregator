b90b092a9e7b52de394e624141d6779134496dc3
Richard Thoya <rkthoya@gmail.com>
2022-07-20 11:35:36 +0300

    Fix Episode model

--- aggregator/admin.py
+++ aggregator/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Content
+from .models import Episode
 
 
-admin.site.register(Content)
+admin.site.register(Episode)
--- aggregator/models.py
+++ aggregator/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Content(models.Model):
+class Episode(models.Model):
     podcast_name = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     description = models.TextField()
