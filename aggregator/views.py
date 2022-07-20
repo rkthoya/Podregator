29ed44e3d663bd332cdc871fd20d664a7024418f
Richard Thoya <rkthoya@gmail.com>
2022-07-20 09:57:44 +0300

    Add home page view

--- aggregator/views.py
+++ aggregator/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Content
+
+
+class HomePageView(ListView):
+    template_name = "homepage.html"
+    model = Content
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["items"] = Content.objects.filter().order_by("-pub_date")[:20]
+        return context
